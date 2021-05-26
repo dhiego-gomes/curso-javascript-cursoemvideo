1f6d269b5c1436e2d83e743a5705cc0fe4415f68
Dhiego Gomes <dhiego.jw@gmail.com>
2021-05-26 16:48:30 -0300

    :rocket: Envia aula 11

---
+++ aula11/ex008.js
@@ -0,0 +1,10 @@
+var vel = 78.2
+
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+
+//  Condição SIMPLES
+if (vel > 60) {
+    console.log('Você ultrapassou o limite de velocidade. MULTADO!')
+}
+
+console.log('Dirija sempre usando cinto de segurança.')
