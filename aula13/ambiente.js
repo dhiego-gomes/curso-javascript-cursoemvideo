dc4ae369daa06cf6d46b273a142aad8a58baeaaa
Dhiego Gomes <dhiego.jw@gmail.com>
2021-06-17 15:48:27 -0300

    :rocket: Envia aulas 13 e 14

---
+++ aula13/ambiente.js
@@ -0,0 +1,17 @@
+var cont = 1
+// while (cont <= 10) {
+//     console.log(`Passo ${cont}`)
+//     cont++
+// }
+
+do {
+    console.log('Tudo bem?')
+    cont++
+} while (cont <= 10)
+
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
